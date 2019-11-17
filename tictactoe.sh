4699ecdcbdfc4b78ddab0c50713d39e07088a11e
luckyNB <20150773@dbatu.ac.in>
2019-11-17 13:13:26 +0530

    Symbol assignment to player either X or O

--- tictactoe.sh
+++ tictactoe.sh
@@ -4,6 +4,7 @@
    declare MAXBOARDSIZE=9
    #variables
    position=0
+   playerSymbol=''
    # TicTacToe Board
    declare -a Board
 	 function initializingBoard(){
@@ -13,5 +14,20 @@
           done 	
        }
 
+   function assigningSymbol(){
+     
+      symbol=$((RANDOM%2))
+       if [ $symbol -eq $] 
+       then
+           playerSymbol='X'
+       else
+	   playerSymbol='O'
+       fi
+      echo "Player has got symbol:==>$playerSymbol"
+ }
+
+
+
   initializingBoard
+  assigningSymbol
 
