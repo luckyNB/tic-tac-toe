601c5ddd1563b06554b696605b4812fbbd660bb2
luckyNB <20150773@dbatu.ac.in>
2019-11-18 10:38:42 +0530

    refacored and given proper Identation

--- tictactoe.sh
+++ tictactoe.sh
@@ -7,27 +7,23 @@
    playerSymbol=''
    # TicTacToe Board
    declare -a Board
-	 function initializingBoard(){
+	function initializingBoard(){
           for((position=1;position<=9;position++))
           do
-                Board[position]=0            
+	                Board[position]=0            
           done 	
        }
 
    function assigningSymbol(){
-     
-      symbol=$((RANDOM%2))
-       if [ $symbol -eq 1 ] 
-       then
-           playerSymbol='X'
-       else
-	   playerSymbol='O'
+       symbol=$((RANDOM%2))
+ 	if [ $symbol -eq 1 ] 
+        then
+        	   playerSymbol='X'
+        else
+		   playerSymbol='O'
        fi
-      echo "Player has got symbol:==>$playerSymbol"
+      	echo "Player has got symbol:==>$playerSymbol"
  }
-
-
-
   initializingBoard
   assigningSymbol
 
